214c6557b2cf3802c4652676740c1e79d6b12b81
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-03-12 12:09:07 -0700

    . and .. handling for ajaxImages
    Assorted Fixes

--- COWS TV Display/includes/ajaxImages.php
+++ COWS TV Display/includes/ajaxImages.php
@@ -7,11 +7,14 @@
 if (!isset($_GET['callback'])) exit(0);
 
 $out = array();
-$handle = opendir("/var/www/cows/images");
+$handle = opendir("/var/www/cows/images/");
 
 while (false !== ($entry = readdir($handle))) {
-	array_push($out,$entry);
+	if ($entry != "." && $entry != "..") {
+		array_push($out,$entry);
+	}
 }
+closedir($handle);
 $json = json_encode($out);
 
 echo $_GET['callback'] . "($json);";
